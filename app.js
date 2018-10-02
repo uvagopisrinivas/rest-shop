c663aca07cf93eebd50ba19cd0628911a13c20c3
UvaGopiSrinivas Kondisetty <C-UvaGopiSrinivas.Kondisetty@charter.com>
2018-10-01 20:19:44 -0400

    Nodemon Atomatic way to spin server

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const productRoutes =  require('./api/routes/products.js');
 const orderRoutes = require('./api/routes/orders.js');
-app.use('/products', productRoutes); // If URL has products then handled by productsRoutes 
+app.use('/products', productRoutes); // If URL has products then it is handled by productsRoutes 
 app.use('/orders', orderRoutes);
 
 /* Incoming request has to go through app.use('request')
