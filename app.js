8c89153fcbb035c1ef8213728a6c3d210860e005
UvaGopiSrinivas Kondisetty <C-UvaGopiSrinivas.Kondisetty@charter.com>
2018-10-01 20:30:47 -0400

    cleanUp

--- app.js
+++ app.js
@@ -6,15 +6,4 @@ const orderRoutes = require('./api/routes/orders.js');
 app.use('/products', productRoutes); // If URL has products then it is handled by productsRoutes 
 app.use('/orders', orderRoutes);
 
-/* Incoming request has to go through app.use('request')
- * Next is used to pass the request to the next handler in line 
- */
-// app.use((req, res, next) => {
-//     res.status(200).json({
-//         message: 'It works'
-//     }); 
-// });
-
-// ^ sample code just to explain how the request is handled by use command 
-
 module.exports = app;
