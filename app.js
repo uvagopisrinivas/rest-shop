7f87de7c83b50fa4b6e24834f889cfa7c2d6db4d
UvaGopiSrinivas Kondisetty <C-UvaGopiSrinivas.Kondisetty@charter.com>
2018-10-02 10:39:47 -0400

    body-parser NodeModules installation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 app.use(morgan('dev')); // logs the data 
 
 const productRoutes =  require('./api/routes/products.js');
